cdf7b489db218d664c7cea9186c57b962cba24ef
Andres <aferrod0406@g.educaand.es>
2024-11-19 12:07:01 +0100

    Actualizaciones

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Canvas from "./Canvas";
 
 export default function Home() {
   return (
@@ -12,13 +13,14 @@ export default function Home() {
       <br />
       <br />
       <h1>Ejercicio 2</h1>
+      <Canvas />
       <br />
       <br />
       <br />
       <br />
       <br />
       <h1>Ejercicio 3</h1>
-   
+      
 
     </div>
   );
